43a6179c4a19c5e213623c9d75946ff4d2724f6b
ddddurk <ddddurk@proton.me>
2023-04-11 10:11:45 -0400

    chore(@dddstack/droplet-copy): create package

---
+++ packages/droplet-copy/src/copy.ts
@@ -0,0 +1,18 @@
+import { log } from "@dddstack/droplet-log";
+import { copyFileSync, existsSync, lstatSync, mkdirSync, readdirSync } from "fs";
+import { join } from "path";
+
+export const copy = (copyDirectory: string, toDirectory: string) =>
+  readdirSync(copyDirectory).map((dirFile) => {
+    const copyDirectoryDirFile = join(copyDirectory, dirFile);
+    const toDirectoryDirFile = join(toDirectory, dirFile);
+
+    if (lstatSync(copyDirectoryDirFile).isDirectory())
+      return copy(copyDirectoryDirFile, toDirectoryDirFile);
+
+    if (!existsSync(toDirectory)) mkdirSync(toDirectory, { recursive: true });
+
+    copyFileSync(copyDirectoryDirFile, toDirectoryDirFile);
+
+    log({ file: toDirectoryDirFile, type: "file" });
+  });
