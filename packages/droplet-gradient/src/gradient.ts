77ee334116f8977e9123b614e421963cd8ad0036
ddddurk <ddddurk@proton.me>
2023-04-11 10:01:57 -0400

    chore(@dddstack/droplet-gradient): create package

---
+++ packages/droplet-gradient/src/gradient.ts
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+import gradientString from "gradient-string";
+
+export const gradient = () =>
+  console.log(chalk.bold(gradientString(["#33FFDC", "#33EAFF"])("💧 DROPLET")));
