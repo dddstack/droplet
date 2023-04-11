d9b5cb8c6e721db3944340a071bd1a129efa5c9f
ddddurk <ddddurk@proton.me>
2023-04-11 11:50:58 -0400

    fix(@dddstack/droplet-logs): fix error, status logs

--- packages/droplet-log/src/log/logError.ts
+++ packages/droplet-log/src/log/logError.ts
@@ -2,4 +2,4 @@ import chalk from "chalk";
 import { Log } from "../log.types";
 
 export const logError = (log: Log<"error">) =>
-  console.error(chalk.red(log.message));
+  console.error(`${chalk.red("ERROR")} ${log.message}`);
--- packages/droplet-log/src/log/logStatus.ts
+++ packages/droplet-log/src/log/logStatus.ts
@@ -3,4 +3,4 @@ import chalk from "chalk";
 import { Log } from "../log.types";
 
 export const logStatus = (log: Log<"status">) =>
-  console.log(`${chalk.yellow(log.command)} [${log.statusOf}/${log.status}] ${log.message}`);
+  console.log(`${chalk.yellow(log.command)} [${log.status}/${log.statusOf}] ${log.message}`);
