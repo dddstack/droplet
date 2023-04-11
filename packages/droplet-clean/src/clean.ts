7712c8016b314723eea5ba5f7e0e228f26777bf4
ddddurk <ddddurk@proton.me>
2023-04-11 09:47:16 -0400

    chore(@dddstack/droplet-clean): create package

---
+++ packages/droplet-clean/src/clean.ts
@@ -0,0 +1,10 @@
+import {
+  DROPLET_DIRECTORY_PREFIX,
+  DROPLET_FILE_PREFIX,
+  DROPLET_TEMPLATE_PREFIX
+} from "@dddstack/droplet-constants";
+
+export const clean = (path: string) => path
+  .replace(new RegExp(DROPLET_DIRECTORY_PREFIX, "g"), "")
+  .replace(new RegExp(DROPLET_FILE_PREFIX, "g"), "")
+  .replace(new RegExp(DROPLET_TEMPLATE_PREFIX, "g"), "");
---
+++ packages/droplet-clean/tests/clean.test.ts
@@ -0,0 +1,13 @@
+import { DROPLET_TEMPLATE_PREFIX } from "@dddstack/droplet-constants";
+
+import { clean } from "../src";
+
+describe("clean", () =>
+  it("should clean a path", () =>
+    expect(
+      clean(
+        `${DROPLET_TEMPLATE_PREFIX}droplet_directory_nested/droplet_directory_nested/droplet_template_template`
+      )
+    ).toEqual("nested/nested/template")
+  )
+);
