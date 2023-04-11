5a76972ce5d66614bb2829abbeb6a30f46d4a2fc
ddddurk <ddddurk@proton.me>
2023-04-11 11:50:37 -0400

    feat(@dddstack/droplet-list-templates): include cleaned, original, trimmed, and trimmedCleaned template paths

--- packages/droplet-list-templates/src/listTemplates.ts
+++ packages/droplet-list-templates/src/listTemplates.ts
@@ -1,14 +1,26 @@
+import { clean } from "@dddstack/droplet-clean";
 import { DROPLET_DIRECTORY_NAME, DROPLET_TEMPLATE_PREFIX } from "@dddstack/droplet-constants";
+import { trim } from "@dddstack/droplet-trim";
 import { lstatSync, readdirSync } from "fs";
 import { join } from "path";
 
-export const listTemplates = (path: string): string[] =>
+export const listTemplates = (path: string, rootPath?: string): {
+  cleaned: string;
+  original: string;
+  trimmed: string;
+  trimmedCleaned?: string;
+}[] =>
   readdirSync(path).map((pathPath) => {
     const joinedPathPathPath = join(path, pathPath);
 
     if (lstatSync(joinedPathPathPath).isDirectory())
       return pathPath.startsWith(DROPLET_TEMPLATE_PREFIX)
-        ? joinedPathPathPath
+        ? {
+          cleaned: clean(pathPath),
+          original: joinedPathPathPath,
+          trimmed: trim(joinedPathPathPath, rootPath ?? path),
+          trimmedCleaned: clean(trim(joinedPathPathPath, rootPath ?? path))
+        }
         : pathPath.startsWith(DROPLET_DIRECTORY_NAME)
           ? listTemplates(joinedPathPathPath)
           : [];
