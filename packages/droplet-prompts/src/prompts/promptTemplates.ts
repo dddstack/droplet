cf8982ae2ed0799a0a08fbeb9d15ac35caf25064
ddddurk <ddddurk@proton.me>
2023-04-11 11:51:43 -0400

    feat(@dddstack/droplet-prompts): include trimmedCleaned in promptTemplates

--- packages/droplet-prompts/src/prompts/promptTemplates.ts
+++ packages/droplet-prompts/src/prompts/promptTemplates.ts
@@ -3,7 +3,7 @@ import { PromptObject } from "prompts";
 
 export const promptTemplates = (templateChoices: ReturnType<typeof listTemplates>): PromptObject => ({
   choices: templateChoices.map((templateChoice) => ({
-    title: templateChoice,
+    title: templateChoice.trimmedCleaned,
     value: templateChoice
   })),
   instructions: false,
