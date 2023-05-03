2fbd8bae5c700e18d2fd9193f5a9464898be4ae1
ddddurk <ddddurk@proton.me>
2023-05-02 22:15:49 -0400

    feat(apps): create docs

---
+++ apps/docs/src/lib/data.ts
@@ -0,0 +1,6 @@
+export const data = {
+  description:
+    "A simple code generation tool for complex codebases.",
+  github: "https://github.com/dddstack/droplet",
+  title: "@dddstack/droplet"
+};
